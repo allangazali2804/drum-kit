322da1e578da0c1f036bb86cd559d0036a41478e
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-09-22 17:37:48 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var numberOfButton = document.querySelectorAll(".drum").length;
+// - DRUM BUTTON START //
+var drum = document.querySelectorAll(".drum");
+var numberOfButton = drum.length;
+
+
+
 for(let i = 0; i < numberOfButton; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!");
-    });
+    drum[i].addEventListener("click", play);
 }
+
+function play() {
+    this.innerHTML = "";
+    var audio = new Audio("./sounds/tom-1.mp3");
+    audio.play();
+}
+
+// - DRUM BUTTON END - //
+
+
 /*
 for(let i = 0; i < numberOfButton; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-}
+} 
 
 function handleClick() {
     alert("I got clicked!");
