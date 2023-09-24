366ae36a5db76588d91a00801beec249787d2459
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-09-24 20:20:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-// - DRUM BUTTON START //
-var drum = document.querySelectorAll(".drum");
-var numberOfButton = drum.length;
+// - DRUM BUTTON START - //
 
-
-
-for(let i = 0; i < numberOfButton; i++){
-    drum[i].addEventListener("click", play);
+var drumButton = document.querySelectorAll(".drum");
+for(let i = 0; i < drumButton.length; i++){
+    drumButton[i].addEventListener("click", play);
 }
 
 function play() {
-    this.innerHTML = "";
-    var audio = new Audio("./sounds/tom-1.mp3");
-    audio.play();
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+        case "w":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kickBass = new Audio("./sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+        case "s":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        default:
+            break;
+    }
+    
 }
 
 // - DRUM BUTTON END - //
+
 
 
 /*
